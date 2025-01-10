a1ee2e3d4473edef26833fbf4ddca9dd90e3d537
SihaoCheng123 <sihao.cheng@estudiante.epsum.school>
2025-01-10 10:55:49 +0100

    hehe

--- app/presentation/views/auth/Registro.tsx
+++ app/presentation/views/auth/Registro.tsx
@@ -1,10 +1,67 @@
 import * as React from 'react';
-import {Text, View} from "react-native";
+import {Image, Text, TextInput, ToastAndroid, TouchableOpacity, View} from "react-native";
+import styles from "./StylesRegistro";
 
 function RegistroScreen() {
     return (
-        <View>
-            <Text>Página de registro</Text>
+        <View style={styles.container}>
+
+            <View style={styles.imageContainer}>
+                <Image source={require('../../../../assets/logo.png')} style={styles.image}></Image>
+
+                <Text style={styles.title}>First App</Text>
+            </View>
+
+            <View style={styles.formContainer}>
+                <Text style={styles.titleForm}>REGISTRO</Text>
+                <View>
+                    <TextInput style={styles.formInput}
+                               placeholder={"Usuario"}
+                               keyboardType={"default"}
+                               secureTextEntry={false}
+                    ></TextInput>
+                </View>
+
+                <View>
+                    <TextInput style={styles.formInput}
+                               placeholder={"Contraseña"}
+                               keyboardType={"default"}
+                               secureTextEntry={true}
+                    ></TextInput>
+                </View>
+
+                <View>
+                    <TextInput style={styles.formInput}
+                               placeholder={"Nombre"}
+                               keyboardType={"default"}
+                               secureTextEntry={false}
+                    ></TextInput>
+                </View>
+
+                <View>
+                    <TextInput style={styles.formInput}
+                               placeholder={"Edad"}
+                               keyboardType={"numeric"}
+                               secureTextEntry={false}
+                    ></TextInput>
+                </View>
+
+                <View>
+                    <TextInput style={styles.formInput}
+                               placeholder={"Correo"}
+                               keyboardType={"email-address"}
+                               secureTextEntry={false}
+                    ></TextInput>
+                </View>
+
+                <View style={styles.formButton}>
+                    <TouchableOpacity onPress={() =>{ToastAndroid.show("Registrado correctamente", ToastAndroid.LONG)}}>
+                        <Text style={{color: "white", textAlign: "center"}}>Registrar</Text>
+                    </TouchableOpacity>
+                </View>
+
+            </View>
+
         </View>
     );
 }
---
+++ app/presentation/views/auth/StylesRegistro.tsx
@@ -0,0 +1,64 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: "black"
+    },
+    title: {
+        color: "white",
+        textAlign: "center",
+        marginTop: "4%",
+        fontSize: 20,
+        fontWeight: "bold"
+    },
+    imageContainer: {
+        alignSelf: "center",
+        marginTop: 90,
+    },
+    image:{
+        width: 125,
+        height: 125,
+    },
+    formContainer:{
+        width: '95%',
+        backgroundColor: "white",
+        paddingVertical: 20,
+        paddingHorizontal: 15,
+        marginHorizontal: "auto",
+        marginTop: 80,
+        borderRadius: 10,
+    },
+    titleForm:{
+        marginBottom: 18,
+        fontWeight: "bold",
+        fontSize: 15,
+        borderBottomWidth: 1,
+        paddingBottom: 9,
+        textAlign: "center",
+        color: "#242424",
+        borderBottomColor: "#242424"
+    },
+    formInputContainer:{
+        marginBottom: 18,
+    },
+    formInput:{
+        borderWidth: 1,
+        borderColor: "#242424",
+        paddingVertical: 7,
+        paddingHorizontal: 8,
+        borderRadius: 5,
+        marginBottom: 20
+    },
+    formButton:{
+        backgroundColor: "#242424",
+        color: "#ffffff",
+        textAlign: "center",
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        borderRadius: 5
+    },
+});
+
+export default styles;
